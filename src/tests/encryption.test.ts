9c1c4b4b0c48fef583e51c10bd92c5a04a5db28a
Adriano Souza Costa <adriano.souza@consultaremedios.com.br>
2019-11-19 14:26:12 -0300

    update unit tests

--- src/tests/encryption.test.ts
+++ src/tests/encryption.test.ts
@@ -2,21 +2,33 @@
 import encryption from './../utils/encryption'
 
 test('Hash should not equals to value', async () => {
-  const value = '1234567'
-  const hash = await encryption.hash(value)
-  expect(hash).not.toBe('1234567')
+  try {
+    const value = '1234567'
+    const hash = await encryption.hash(value)
+    expect(hash).not.toBe('1234567')
+  } catch (error) {
+    fail(error)
+  }
 })
 
 test('Hash comparison with origin value should return true', async () => {
-  const value = 'ab*553'
-  const hash = await encryption.hash(value)
-  const compare = await encryption.compare(value, hash)
-  expect(compare).toBeTruthy()
+  try {
+    const value = 'ab*553'
+    const hash = await encryption.hash(value)
+    const compare = await encryption.compare(value, hash)
+    expect(compare).toBeTruthy()
+  } catch (error) {
+    fail(error)
+  }
 })
 
 test('Hash comparison with false origin value should return false', async () => {
-  const value = 'ab*553'
-  const hash = await encryption.hash(value)
-  const compare = await encryption.compare('ab**ˆ%%', hash)
-  expect(compare).toBeFalsy()
+  try {
+    const value = 'ab*553'
+    const hash = await encryption.hash(value)
+    const compare = await encryption.compare('ab**ˆ%%', hash)
+    expect(compare).toBeFalsy()
+  } catch (error) {
+    fail(error)
+  }
 })
--- src/tests/tokenManager.test.ts
+++ src/tests/tokenManager.test.ts
@@ -2,22 +2,45 @@
 import tokenManager from './../utils/components/tokenManager'
 
 test('Should generate token hash and time expiration', async () => {
-  const user = { id: 'ab123', name: 'Adriano' }
-  const { token, expiresIn } = tokenManager.sign(user)
-  expect(token).not.toBe(user)
-  expect(token).not.toBeNull()
-  expect(expiresIn).not.toBeNull()
+  try {
+    const user = { id: 'ab123', name: 'Adriano' }
+    const { token, expiresIn } = tokenManager.sign(user)
+    expect(token).not.toBe(user)
+    expect(token).not.toBeNull()
+    expect(expiresIn).not.toBeNull()
+  } catch (error) {
+    fail(error)
+  }
 })
 
 test('Extract correctly id from token hash', async () => {
-  const user = { id: 'ab123', name: 'Adriano' }
-  const { token } = tokenManager.sign(user)
-  const tokenData = tokenManager.verify(token)
-  expect(tokenData.id).toBe('ab123')
+  try {
+    const user = { id: 'ab123', name: 'Adriano' }
+    const { token } = tokenManager.sign(user)
+    const tokenData = tokenManager.verify(token)
+    expect(tokenData.id).toBe('ab123')
+  } catch (error) {
+    fail(error)
+  }
 })
 
 test('Extract correctly user id from token hash', async () => {
-  const { token } = tokenManager.signUser('ab123')
-  const tokenData = tokenManager.verify(token)
-  expect(tokenData.id).toBe('ab123')
+  try {
+    const { token } = tokenManager.signUser('ab123')
+    const tokenData = tokenManager.verify(token)
+    expect(tokenData.id).toBe('ab123')
+  } catch (error) {
+    fail(error)
+  }
+})
+
+test('Must be not extract data from invalid token', async () => {
+  const user = { id: 'ab123', name: 'Adriano' }
+  const { token } = tokenManager.sign(user)
+  try {
+    tokenManager.verify(token + 'sdfsdf')
+    fail()
+  } catch {
+    expect(true)
+  }
 })
