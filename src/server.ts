6d85378d378801f2484a2a996734ed742bfe0330
Adriano Souza Costa <adriano.souza@consultaremedios.com.br>
2020-01-03 19:33:52 -0300

    Update files

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app'
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3333
 
 app.listen(port, () => {
   console.log(`App listening on the port ${port}`)
