218c2ee6082ef7277c2ded22b3842ac7be1e29fe
Adriano Souza Costa <adriano.souza@consultaremedios.com.br>
2020-01-03 17:13:53 -0300

    Atualizando versão

--- dist/exceptions/notImplemented.exception.js
+++
@@ -1,9 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _errorexception = require('./error.exception'); var _errorexception2 = _interopRequireDefault(_errorexception);
-
-class NotImplemetedException extends _errorexception2.default {
-  constructor () {
-    super('Method not implemented.', 501)
-  }
-}
-
-exports. default = new NotImplemetedException()
