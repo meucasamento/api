03918c2a1f195a277985670c8ead20a5f4a58192
Adriano Souza Costa <adrianocosta@Adrianos-MacBook-Pro.local>
2020-01-08 00:15:26 -0300

    Voltando com código antigo

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,43 @@
-import express, { Router } from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import bodyParser from 'body-parser'
+import express, { json } from 'express'
+
+import Database from './database/database'
+import RepositoryFactoryInteface from './factories/v1/repository.factory.interface.v1'
+import RoutesV1 from './routes/v1/router.v1'
+import ErrorMiddleware from './middlewares/v1/error.middleware.v1'
+import RouterInterface from './routes/router.interface'
+
+import MailServiceInterface from './utils/components/mail/mail.service.interface'
 
 class App {
-  public express: express.Application
-  private routes = Router()
+  private express: express.Application
+  private routerV1: RouterInterface
 
-  public constructor () {
+  constructor (repositoryFactory: RepositoryFactoryInteface,
+    emailService: MailServiceInterface) {
     this.express = express()
-    this.routes.get('/users', (req, res) => {
-      res.json({ name: 'Adriano Souza Costa' })
-    })
-    this.express.use(this.routes)
-    this.express.use(express.json())
+    this.routerV1 = new RoutesV1(repositoryFactory, emailService)
+  }
+
+  public setup (): void {
+    Database.setup()
+    this.middlewares()
+    this.routes()
+  }
+
+  private middlewares (): void {
+    this.express.use(helmet())
+    this.express.use(json())
+    this.express.use(cors())
+    this.express.use(bodyParser.json())
+    this.express.use(ErrorMiddleware.checkError)
+  }
+
+  private routes (): void {
+    this.express.use('/api/v1', this.routerV1.router)
   }
 }
 
-export default new App().express
+export default App
